9a940452c4fb925806eaaa4f9228c1f66603f18b
mattmuroya <matt.muroya@gmail.com>
2022-03-10 19:46:32 -1000

    basic homepage layout and component initialization

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import FadeIn from "react-fade-in/lib/FadeIn";
+
+export default function NavBar() {
+  return (
+    <main className="home-main">
+      <FadeIn className="hero-container" delay={300} transitionDuration={1000}>
+        <h2 className="hero-text">Shop Our Lens Selection.</h2>
+        <button className="shop-btn">Shop Now</button>
+      </FadeIn>
+    </main>
+  );
+};
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <div id="logo">The Lens Shop</div>
+        <ul id="menu">
+          <li>Home</li>
+          <li>Shop</li>
+          <li>Cart (0)</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
