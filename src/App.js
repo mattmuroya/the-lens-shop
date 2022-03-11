8161dfd0539f683090d9e17ac5b3a82cddd0cfc5
mattmuroya <matt.muroya@gmail.com>
2022-03-10 20:13:52 -1000

    add basic routing and catcher

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="wrapper">
       <NavBar />
-      <Home />
       <Outlet />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import App from './App';
 import {
   BrowserRouter,
   Routes,
   Route,
- } from 'react-router-dom';
+} from 'react-router-dom';
+import App from './App';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Cart from './components/Cart';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={
+            <main className="error">
+              <div className="error-wrapper">
+                <p>There's nothing here!</p>
+                <p><a href="/">Go Home</a></p>
+              </div>
+            </main>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
