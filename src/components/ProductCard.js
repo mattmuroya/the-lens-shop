b268dd4844d4720fa93284b187cf4841be319a0e
mattmuroya <matt.muroya@gmail.com>
2022-03-11 21:58:52 -1000

    implement json fetch and shop page render

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ export default function ProductCard() {
       <div className="description">
         <h3>XF16mmF1.5 R WR</h3>
         <form>
-          <label for="quantity">Quantity</label>
+          <label htmlFor="quantity">Quantity</label>
           <input type="number" id="quantity" name="quantity" min="0" defaultValue="1" />
           <button type="submit" onClick={(e) => e.preventDefault()}>Add to Cart</button>
         </form>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,31 @@
-import FadeIn from "react-fade-in/lib/FadeIn";
-import ProductCard from "./ProductCard";
+import { useEffect, useState } from 'react';
+import FadeIn from 'react-fade-in/lib/FadeIn';
+import ProductCard from './ProductCard';
+
 
 export default function Shop() {
+
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    try {
+      const response = await fetch('/data/lenses.json');
+      const data = await response.json();
+      setProducts(data);
+    } catch (err) {
+      alert(`${err}. Please contact the administrator of this page.`);
+    }
+  }
+
+  useEffect(() => { // calls getProducts() on mount
+    getProducts();
+  }, []);
+
+
   return (
     <main className="shop-main">
         <FadeIn className="shop-container">
-          <ProductCard />
+          {products.map(product => <div key={product.id} className="product-card">{product.name}</div>)}
         </FadeIn>
     </main>
   );
