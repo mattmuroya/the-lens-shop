0eb192e8bbaeeffcd51d1fac71f25e7a781f9941
mattmuroya <matt.muroya@gmail.com>
2022-03-12 20:11:39 -1000

    styling and layout for cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,23 @@
 export default function Cart() {
   return (
-    <main>
-      Cart
+    <main className="cart-main">
+      <div className="cart-container">
+        <div className="cart-items">
+          <div className="cart-product">
+            <div className="cart-product-name">XF16mmF1.4 R WR</div>
+            <div>Quantity: <input type="number" value="1" /></div>
+          </div>
+          <div className="cart-product">
+            <div className="cart-product-name">XF23mmF1.4 R</div>
+            <div>Quantity: <input type="number" value="2" /></div>
+          </div>
+          <div className="cart-product">
+            <div className="cart-product-name">XF35mmF1.4 R</div>
+            <div>Quantity: <input type="number" value="3" /></div>
+          </div>
+          <button>Check Out</button>
+        </div>
+      </div>
     </main>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <main className="home-main">
       <FadeIn className="hero-container" delay={300} transitionDuration={1000}>
-        <h2 className="hero-text">Shop Our Lens Selection.</h2>
+        <h2 className="hero-text">View Our Lens Selection.</h2>
         <button className="shop-btn" onClick={() => window.location='/shop'} type="button">Shop Now</button>
       </FadeIn>
     </main>
