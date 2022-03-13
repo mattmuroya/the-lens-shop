4ed0e86bf2c89746bd08cc1686ca5a225708d839
mattmuroya <matt.muroya@gmail.com>
2022-03-12 19:38:46 -1000

    style product page component

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -5,11 +5,10 @@ export default function ProductPage(props) {
 
   const [products] = useProducts('/data/lenses.json');
 
-
   const { productId } = useParams();
 
-
   const {
+    id,
     name,
     aperture,
     'focal length':focalLength,
@@ -18,15 +17,24 @@ export default function ProductPage(props) {
     return product.id === productId;
   }) || {}; // fallback empty object for when value is undefined prior to fetch
 
-
   return (
     <main className="product-page-main">
-      <div className="product">
-        <img className="product-img" />
+      <div className="product-page-container">
+        <img className="product-img" src={`/img/${id}.jpg`} alt={name} />
         <div className="product-details">
-          <h2>{name}</h2>
-          <p>Focal length: {focalLength}</p>
-          <p>Maximum aperture: {aperture}</p>
+          <div className="content-container">
+            <h2>{name}</h2>
+            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus dictum at. Viverra orci sagittis eu volutpat odio facilisis.</p>
+            <p>Focal length: {focalLength}</p>
+            <p>Maximum aperture: {aperture}</p>
+            <p>Price: ${price} USD</p>
+            <form>
+              <label htmlFor="quantity">Quantity</label>
+              <input type="number" id="quantity" name="quantity" min="0" defaultValue="1" />
+              <button type="submit" onClick={(e) => e.preventDefault()}>Add to Cart</button>
+            </form>
+            <a href="/shop">Return to Shop Page</a>
+          </div>
         </div>
       </div>
     </main>
